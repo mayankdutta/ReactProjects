901b4ca8bf3491c196ab3afcbc74c467be7276c0
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-05-07 13:49:40 +0530

    lesson learnt, mapping json values in modern react

--- social-media-cards/src/App.js
+++ social-media-cards/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>props.id</h1>
+      <h1>props.title</h1>
+      <h1>props.poster</h1>
+      <h1>props.overview</h1>
+      <h1>props.release_date</h1>
+      <h1>props.genres</h1>
     </div>
   );
 }
--- social-media-cards/src/index.js
+++ social-media-cards/src/index.js
@@ -1,16 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import data from "./Database/data";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Card(props) {
+  const card_style = {
+    width: "18rem",
+  };
+  return (
+    <div class='card' style={card_style}>
+      <img src={props.poster} class='card-img-top' alt='...' />
+      <div class='card-body'>
+        <h5 class='card-title'>{props.title}</h5>
+        <p class='card-text'>{props.overview}</p>
+      </div>
+    </div>
+  );
+}
+
+const Grid_it = {
+  display: "grid",
+  gridTemplateColumns: "auto auto auto",
+  gridRowGap: "2rem",
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className='display-1'>Hello there</div>
+    <div className='container' style={Grid_it}>
+      {data.map((d) => {
+        return (
+          <Card
+            id={d.id}
+            title={d.title}
+            poster={d.poster}
+            overview={d.overview}
+          />
+        );
+      })}
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
